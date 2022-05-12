6ab40fa12972d2ca305a33ede7a38f1449182f6c
Gunther <gunt.valentine@gmail.com>
2022-05-12 19:36:00 -0400

    commit tree 14a2cc11d89ba40d2283ad18d41b810b9ad0eceb

--- index.js
+++ index.js
@@ -21,3 +21,71 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array) {
+    let employeeInfo = {}
+    employeeInfo.firstName = array[0];
+    employeeInfo.familyName = array[1];
+    employeeInfo.title = array[2];
+    employeeInfo.payPerHour = array[3];
+    employeeInfo.timeInEvents = [];
+    employeeInfo.timeOutEvents = [];
+    return employeeInfo;
+}
+
+function createEmployeeRecords(arrays) {
+    let a = arrays.map(array => {
+        return createEmployeeRecord(array)
+    })
+    return a;
+}
+
+function createTimeInEvent(dateTime) {
+    let newArray = dateTime.split(" ");
+    let date = newArray[0]
+    let hour = parseInt(newArray[1])
+    let timeIn = {
+        type: "TimeIn" ,
+        hour: hour ,
+        date: date
+    }
+    this.timeInEvents.push(timeIn)
+    return this
+}
+
+function createTimeOutEvent(dateTime) {
+    let newArray = dateTime.split(" ");
+    let date = newArray[0]
+    let hour = parseInt(newArray[1])
+    let timeOut = {
+        type: "TimeOut" ,
+        hour: hour ,
+        date: date
+    }
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(dateTime) {
+    let timeIn = this.timeInEvents.find(event => event.date === dateTime)
+    let timeOut = this.timeOutEvents.find(event => event.date === dateTime)
+    return (timeOut.hour - timeIn.hour) / 100  
+}
+
+function wagesEarnedOnDate(dateTime) {
+    return hoursWorkedOnDate.call(this, dateTime) * this.payPerHour
+}
+
+function findEmployeeByFirstName(arrays, firstName) {
+    let array = {}
+    return arrays.find(array => (firstName === array.firstName))
+}
+
+function calculatePayroll(employeeRecords) {
+    console.log(employeeRecords)
+    let payroll = 0
+    employeeRecords.forEach(employee => {
+    payroll += allWagesFor.call(employee)
+    })
+    console.log(payroll)
+    return payroll
+}
